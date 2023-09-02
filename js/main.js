3c285980bddd83294f06fb16a57f45cbb405a5c5
FrancescoPepi <francescopepi16@gmail.com>
2023-09-02 15:48:57 +0200

    add chat active

--- js/main.js
+++ js/main.js
@@ -8,4 +8,16 @@ createApp({
     };
   },
   methods: {},
+  // created: {
+  //   addUaccess() {
+  //     contacts.forEach((element) => {
+  //       element.access = randomNumber();
+  //       console.log(contacts);
+  //     });
+  //   },
+  // },
 }).mount("#app");
+
+// function randomNumber() {
+//   Math.floor(Math.random() * (12 + 1) - 1);
+// }
